aa0226a32aa603903018d67b99cbf7014cac6369
SlavaHlushko <slavahlushko90@gmail.com>
2022-09-25 12:42:51 +0300

    task-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle"    
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+
+player.on("timeupdate", throttle(onPlay, 1000));
+function onPlay({ seconds }) {
+    localStorage.setItem("LOCALSTORAGE_KEY", seconds);
+};
+
+const initPage = () => {
+    const saveData = localStorage.getItem('LOCALSTORAGE_KEY')
+    if (saveData) {
+        player.setCurrentTime(saveData)
+    }
+}
+
+initPage();
